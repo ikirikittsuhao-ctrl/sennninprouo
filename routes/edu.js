0f9474e1a8adfbee34209716f29d78ccd7bb797d
xianrensugar-ux <xianrensugar@gmail.com>
2026-06-18 18:42:08 +0900

    Create edu.js

---
+++ routes/edu.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+
+router.get('/scratch-edu/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const configUrl = 'https://raw.githubusercontent.com/siawaseok3/wakame/master/video_config.json';
+    const configRes = await fetch(configUrl);
+    const configJson = await configRes.json();
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send(`https://www.youtubeeducation.com/embed/${id}${configJson.params}`);
+  } catch (e) { res.status(500).send("Error"); }
+});
+
+router.get('/kahoot-edu/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const paramUrl = 'https://raw.githubusercontent.com/wista-api-project/auto/refs/heads/main/edu/1.txt';
+    const response = await fetch(paramUrl);
+    const params = await response.text(); 
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send(`https://www.youtubeeducation.com/embed/${id}${params}`);
+  } catch (e) { res.status(500).send("Error"); }
+});
+
+router.get('/nocookie/:id', (req, res) => {
+  res.set('Content-Type', 'text/plain; charset=utf-8');
+  res.send(`https://www.youtube-nocookie.com/embed/${req.params.id}?autoplay=1`);
+});
+
+module.exports = router;
